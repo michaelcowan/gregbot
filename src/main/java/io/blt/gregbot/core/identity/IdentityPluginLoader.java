9cd654acb760c4990bf586c29698fce17d2f2a13
Mike Cowan <mike.c.cowan@gmail.com>
2023-06-13 12:04:46 +0100

    Notes

---
+++ src/main/java/io/blt/gregbot/core/identity/IdentityPluginLoader.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2023 Mike Cowan.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+package io.blt.gregbot.core.identity;
+
+public class IdentityPluginLoader {
+
+//    private final Map<String, IdentityPlugin<?>> plugins = new HashMap<>();
+//
+//    public IdentityPlugin<?> loadPlugin(IdentityProperties properties) throws IOException {
+//        var plugin = plugins.get(properties.plugin().type());
+//        return setup(plugin, properties.plugin());
+//    }
+//
+//    private <T> IdentityPlugin<T> setup(IdentityPlugin<T> plugin, Properties.Plugin properties) throws IOException {
+//        Class<T> type = plugin.propertyType();
+//        // TODO Should the loader inject the secret values?
+//        plugin.load(properties.readPropertiesAs(type));
+//        // TODO Should the loader set the 'out' variables?
+//        return plugin;
+//    }
+
+}
