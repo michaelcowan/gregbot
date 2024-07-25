9babc85a59ac816035808e9c95512e5ae9fba8ca
Mike Cowan <mike.c.cowan@gmail.com>
2024-07-25 14:35:58 +0100

    Add test plugins throwing on function calls

---
+++ src/test/java/io/blt/gregbot/core/plugin/ThrowOnSecretsForPathSecretPlugin.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2023 Mike Cowan.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+package io.blt.gregbot.core.plugin;
+
+import io.blt.gregbot.plugin.PluginException;
+import io.blt.gregbot.plugin.secrets.SecretException;
+import io.blt.gregbot.plugin.secrets.SecretPlugin;
+import java.util.Map;
+
+public class ThrowOnSecretsForPathSecretPlugin implements SecretPlugin {
+
+    public static final String MESSAGE = "secret plugin exception on secretsForPath";
+
+    @Override
+    public void load(Map<String, String> properties) throws PluginException { }
+
+    @Override
+    public Map<String, String> secretsForPath(String path) throws SecretException {
+        throw new SecretException(MESSAGE, null);
+    }
+
+}
---
+++ src/test/java/io/blt/gregbot/core/plugin/ThrowOnVariablesIdentityPlugin.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2023 Mike Cowan.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+package io.blt.gregbot.core.plugin;
+
+import io.blt.gregbot.plugin.PluginException;
+import io.blt.gregbot.plugin.identities.IdentityException;
+import io.blt.gregbot.plugin.identities.IdentityPlugin;
+import java.util.Map;
+
+public class ThrowOnVariablesIdentityPlugin implements IdentityPlugin {
+
+    public static final String MESSAGE = "identity plugin exception on variables";
+
+    @Override
+    public void load(Map<String, String> properties) throws PluginException { }
+
+    @Override
+    public Map<String, String> variables() throws IdentityException {
+        throw new IdentityException(MESSAGE, null);
+    }
+}
