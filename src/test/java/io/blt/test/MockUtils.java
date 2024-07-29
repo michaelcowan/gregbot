501effb63a1590969d3a85688a56d6486ef96446
Mike Cowan <mike.c.cowan@gmail.com>
2024-07-29 23:19:20 +0100

    Add UI entrypoint and empty MainForm

---
+++ src/test/java/io/blt/test/MockUtils.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2024 Mike Cowan.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+package io.blt.test;
+
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+
+import javax.swing.SwingUtilities;
+
+public final class MockUtils {
+
+    private MockUtils() {
+        throw new IllegalAccessError("Utility class should be accessed statically and never constructed");
+    }
+
+    public static ArgumentCaptor<Runnable> captureSwingInvokeLaterWhile(Runnable runnable) {
+        var captor = ArgumentCaptor.forClass(Runnable.class);
+
+        try (var swingUtilities = Mockito.mockStatic(SwingUtilities.class)) {
+            runnable.run();
+
+            swingUtilities.verify(() -> SwingUtilities.invokeLater(captor.capture()));
+        }
+
+        return captor;
+    }
+
+}
