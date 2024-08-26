230fa6a53765b85c1bb57c7cff9bab5011d8fdf2
Mike Cowan <mike.c.cowan@gmail.com>
2024-08-26 12:14:57 +0100

    Add panels package

--- src/main/java/io/blt/gregbot/ui/dialogs/SplashScreen.java
+++ src/main/java/io/blt/gregbot/ui/dialogs/SplashScreen.java
@@ -10,7 +10,7 @@ package io.blt.gregbot.ui.dialogs;
 
 import io.blt.gregbot.ApplicationProperties;
 import io.blt.gregbot.ApplicationResources;
-import io.blt.gregbot.ui.components.ScaledImagePanel;
+import io.blt.gregbot.ui.panels.ScaledImagePanel;
 import java.util.Timer;
 import java.util.TimerTask;
 
